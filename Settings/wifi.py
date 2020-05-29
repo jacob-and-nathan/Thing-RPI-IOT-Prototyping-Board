a780b18f923f64aa9b9f24f4345546d738dd9420
jacob-and-nathan <jacob.e.macleod@gmail.com>
2020-05-29 11:40:03 +0100

    Updates wifi.py - added in code to read keyboard input

--- Settings/wifi.py
+++ Settings/wifi.py
@@ -1,12 +1,20 @@
 import sys
+#Need to make list of dependencies, but keyboard module can be installed with pip3 install getkey. Adafruit circuit python is also needed. Maybe something else to. 
+from getkey import getkey, keys
+#Above doesn't work, use this
+from evdev import InputDevice, categorize, ecodes
 sys.path.append('/home/pi/ThingBoard/')
 
 from OLED_Text import print_to_screen, show, clear
-#import RPI.GPIO as GPIO
-#GPIO.setwarnings(False)
-#GPIO.setmode(GPIO.BCM)
-clear()
-print_to_screen(0, 0, "SCRIPT WORKING", 7, 1)
-show()
 
-print ("Script working")
+#while True:
+ #   clear()
+  #  key = getkey()
+   # print ("Key is " +key)
+    #print_to_screen(0, 0, key.upper(), 7, 1)
+    #show()
+
+device = InputDevice("/dev/input/event2")
+for event in device.read_loop():
+    if event.type == ecodes.EV_KEY:
+        print (categorize(event))
